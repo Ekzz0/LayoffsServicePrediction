d4813e474b9fe61bada7165f0eb4e48d23017451
Алексей <zaykuh@gmail.com>
2023-12-10 18:22:21 +0500

    feat: update ui kit

--- APP/src/main/resources/static/js/csv.js
+++ APP/src/main/resources/static/js/csv.js
@@ -40,7 +40,10 @@ function postRequest(json){
             throw('Failed in post fetch')
         }
     })
-    .then(setTimeout(getRequest, 5000))
+    .then(data =>{
+        data = JSON.parse(data)
+        setTable(data)
+    })
     .catch(console.error)
 
 }
--- APP/src/main/resources/static/js/table.js
+++ APP/src/main/resources/static/js/table.js
@@ -1,1 +1,21 @@
-console.log(document.getElementById(table))
+persons = [{"id":"1014","probability":0.0003883827,"Group":"Quality Assurance"},{"id":"1044","probability":0.0001027473,"Group":"Sales and Marketing"}];
+setTable(persons)
+
+function setTable(persons){
+    table = document.getElementById('table-body')
+    for (let person of persons){
+        console.log(person)
+        info = []
+        for (let data in person){
+            console.log(person[data])
+            info.push(person[data])
+        }{
+            table.insertAdjacentHTML('beforeend', `<tr>
+            <td class="table__body-id">${info[0]}</td>
+            <td class="table__body-prob">${info[1].toFixed(2)*100}</td>
+            <td class="table__body-group">${info[2]}</td>
+            </tr>`)
+        }
+
+    }
+}
