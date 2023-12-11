4b7a839dbfc923f71988a58db6faf8eadd463ef3
Алексей <zaykuh@gmail.com>
2023-12-11 05:46:53 +0500

    feat: add functional and style for modal window

--- APP/src/main/resources/static/js/table.js
+++ APP/src/main/resources/static/js/table.js
@@ -11,6 +11,7 @@ function setTable(persons){
             <td class="table__body-id">${info[0]}</td>
             <td class="table__body-prob">${(info[1]*100).toFixed(1)}%</td>
             <td class="table__body-group">${info[2]}</td>
+            <td class="table__body-feature">${info[3]}</td>
             </tr>`)
         }
     }
