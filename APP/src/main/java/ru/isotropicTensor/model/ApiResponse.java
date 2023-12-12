fedf9af290538aadc0b05da79203a934113e8efb
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2023-12-13 01:46:05 +0300

    ApiResponse updated according to the ML-api response

--- APP/src/main/java/ru/isotropicTensor/model/ApiResponse.java
+++ APP/src/main/java/ru/isotropicTensor/model/ApiResponse.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApiResponse {
-    private HttpStatus status;
-    private String data;
+    private int status;
+    private List<EmployeePredictionData> data;
 }
