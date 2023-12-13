1cc507cc701042c80638f46c31ad723b2d28e51d
Semen Borinskikh <ph.borinskikh@mail.ru>
2023-12-13 19:03:05 +0300

    add sort by probability

--- ML/test.py
+++ ML/test.py
@@ -8,4 +8,5 @@ model = load_model('./models/XGBoost.pkl')
 feature_construct = load_feature_constructor()
 
 
-
+df = pd.DataFrame([3, 2, 1, 4, 6, 5], columns=['val'])
+print(df.sort_values(by=['val'])[::-1])
