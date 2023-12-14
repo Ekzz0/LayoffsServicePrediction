9a4842f25d7ad1227b41483a7332a8685b265a83
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2023-12-14 13:30:55 +0300

    Added deserializer classes for int and double.

---
+++ APP/src/main/java/ru/isotropicTensor/utils/StringToDoubleDeserializer.java
@@ -0,0 +1,21 @@
+package ru.isotropicTensor.utils;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+
+public class StringToDoubleDeserializer extends JsonDeserializer<Double> {
+
+    @Override
+    public Double deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        String value = jsonParser.getValueAsString();
+        try {
+            return Double.parseDouble(value);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Unable to deserialize value: " + value, e);
+        }
+    }
+}
