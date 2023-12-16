4e48d1480a2138688fd7240d9bfbc734dc90db09
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2023-12-16 22:49:20 +0300

    Changed methods according to api

--- APP/src/main/java/ru/isotropicTensor/service/EmployeeService.java
+++ APP/src/main/java/ru/isotropicTensor/service/EmployeeService.java
@@ -1,10 +1,12 @@
 package ru.isotropicTensor.service;
 
 import ru.isotropicTensor.dto.ApiResponse;
+import ru.isotropicTensor.dto.EmployeePredictsDto;
 import ru.isotropicTensor.utils.EmployeeReportSerializer;
 
 import java.util.List;
 
 public interface EmployeeService {
     ApiResponse getEmployeePredict(List<EmployeeReportSerializer> dataList);
+    EmployeePredictsDto getEmployeePredictsById(int id);
 }
