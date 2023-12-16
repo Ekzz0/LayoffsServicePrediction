04da9ea266915bd843185f6d5ea2f879119788dc
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2023-12-17 02:31:28 +0300

    Added new method for Api endpoint

--- APP/src/main/java/ru/isotropicTensor/service/EmployeeService.java
+++ APP/src/main/java/ru/isotropicTensor/service/EmployeeService.java
@@ -2,6 +2,7 @@ package ru.isotropicTensor.service;
 
 import ru.isotropicTensor.dto.ApiResponse;
 import ru.isotropicTensor.dto.EmployeePredictsDto;
+import ru.isotropicTensor.dto.PredictsDto;
 import ru.isotropicTensor.utils.EmployeeReportSerializer;
 
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.List;
 public interface EmployeeService {
     ApiResponse getEmployeePredict(List<EmployeeReportSerializer> dataList);
     EmployeePredictsDto getEmployeePredictsById(int id);
+    PredictsDto getRecentPredicts();
 }
