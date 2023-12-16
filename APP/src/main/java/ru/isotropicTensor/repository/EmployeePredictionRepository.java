c748aaea45072793b6a538b06e3e1ebe937351f8
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2023-12-17 02:34:18 +0300

    Updated repository method

--- APP/src/main/java/ru/isotropicTensor/repository/EmployeePredictionRepository.java
+++ APP/src/main/java/ru/isotropicTensor/repository/EmployeePredictionRepository.java
@@ -1,7 +1,13 @@
 package ru.isotropicTensor.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.isotropicTensor.model.EmployeePrediction;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface EmployeePredictionRepository extends JpaRepository<EmployeePrediction, Integer> {
+    @Query("SELECT ep.date FROM EmployeePrediction ep GROUP BY ep.date")
+    List<LocalDateTime> findAllByDate();
 }
