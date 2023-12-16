c1e69c66c94af845d7c155321471a39d12e6ddbc
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2023-12-16 11:07:44 +0300

    Predictions are being saved in database now

--- APP/src/main/java/ru/isotropicTensor/service/impl/EmployeeServiceImpl.java
+++ APP/src/main/java/ru/isotropicTensor/service/impl/EmployeeServiceImpl.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.isotropicTensor.dto.ApiResponse;
 import ru.isotropicTensor.model.Employee;
+import ru.isotropicTensor.model.EmployeePrediction;
 import ru.isotropicTensor.model.EmployeeReport;
 import ru.isotropicTensor.repository.EmployeePredictionRepository;
 import ru.isotropicTensor.repository.EmployeeReportRepository;
 import ru.isotropicTensor.repository.EmployeeRepository;
+import ru.isotropicTensor.utils.EmployeePredictionSerializer;
 import ru.isotropicTensor.utils.EmployeeReportSerializer;
 import ru.isotropicTensor.service.EmployeeService;
 
@@ -23,7 +25,6 @@ public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
     private final EmployeeReportRepository employeeReportRepository;
     private final EmployeePredictionRepository employeePredictionRepository;
-
     private final String mlApiURL = "http://localhost:8889/api";
 
     public EmployeeServiceImpl(EmployeeRepository employeeRepository, EmployeeReportRepository employeeReportRepository, EmployeePredictionRepository employeePredictionRepository) {
@@ -54,15 +55,32 @@ public class EmployeeServiceImpl implements EmployeeService {
                 ApiResponse.class);
         ApiResponse apiResponse = response.getBody(); // Получение ответа
 
+        if (apiResponse.getStatus() == HttpStatus.OK.value()) {
+            List<EmployeePrediction> transientPredictions = getTransientPredictionsFromSerializer(apiResponse.getData(),
+                    employees);
+            List<EmployeePrediction> persistantPredictions = employeePredictionRepository.saveAll(transientPredictions);
+        }
+
 
         // TODO: Почитать что RestTemplate возвращает, обработать исключения и так далее.
         return apiResponse; // Отправка результатов предикта в контроллер
     }
 
+    private List<EmployeePrediction> getTransientPredictionsFromSerializer(List<EmployeePredictionSerializer> data,
+                                                                             Map<Integer, Employee> employees) {
+        List<EmployeePrediction> predictions = new ArrayList<>();
+        for (EmployeePredictionSerializer serializer : data) {
+            int employeeId = serializer.getId();
+            predictions.add(buildTransientPrediction(serializer, employees.get(employeeId)));
+        }
+        return predictions;
+    }
+
+
+
     private List<EmployeeReport> getTransientReportsFromSerializer(List<EmployeeReportSerializer> dataList,
                                                                    Map<Integer, Employee> employeeMap) {
         List<EmployeeReport> employeeReports = new ArrayList<>();
-
         for (EmployeeReportSerializer employeeReportSerializer : dataList) {
             int employeeId = employeeReportSerializer.getId();
             employeeReports.add(buildTransientEmployeeReport(employeeReportSerializer, employeeMap.get(employeeId)));
@@ -70,6 +88,34 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employeeReports;
     }
 
+    private Map<Integer, Employee> findOrSaveEmployeesById(List<EmployeeReportSerializer> reportSerializers) {
+        //TODO переделать на оптимальный запрос, где делается одно обращение к бд, а затем сохраняются все сотрудники,
+        // кто не был представлен в бд.
+        Map<Integer, Employee> employees = new HashMap();
+        for (EmployeeReportSerializer reportSerializer : reportSerializers) {
+            if (!employeeRepository.existsById(reportSerializer.getId())) {
+                Employee transientEmployee = Employee.builder()
+                        .id(reportSerializer.getId())
+                        .department(reportSerializer.getDepartment())
+                        .build();
+                Employee persistentEmployee = employeeRepository.save(transientEmployee);
+                employees.put(persistentEmployee.getId(),persistentEmployee);
+            } else {
+                Employee persistentEmployee = employeeRepository.getById(reportSerializer.getId());
+                employees.put(persistentEmployee.getId(),persistentEmployee);
+            }
+        }
+        return employees;
+    }
+
+    private EmployeePrediction buildTransientPrediction(EmployeePredictionSerializer serializer, Employee employee) {
+        return EmployeePrediction.builder()
+                .employee(employee)
+                .date(LocalDateTime.now())
+                .probability(serializer.getProbability())
+                .topFeatures(serializer.getTopFeatures())
+                .build();
+    }
 
     private EmployeeReport buildTransientEmployeeReport(EmployeeReportSerializer employeeReportSerializer,
                                                         Employee employee) {
@@ -102,45 +148,5 @@ public class EmployeeServiceImpl implements EmployeeService {
                 .vacationMentioned(employeeReportSerializer.getVacationMentioned())
                 .politeMessage(employeeReportSerializer.getPoliteMessage())
                 .build();
-    }
-
-    private Map<Integer, Employee> findOrSaveEmployeesById(List<EmployeeReportSerializer> reportSerializers) {
-        //TODO переделать на оптимальный запрос, где делается одно обращение к бд, а затем сохраняются все сотрудники,
-        // кто не был представлен в бд.
-        Map<Integer, Employee> employees = new HashMap();
-        for (EmployeeReportSerializer reportSerializer : reportSerializers) {
-            if (!employeeRepository.existsById(reportSerializer.getId())) {
-                Employee transientEmployee = Employee.builder()
-                        .id(reportSerializer.getId())
-                        .department(reportSerializer.getDepartment())
-                        .build();
-                Employee persistentEmployee = employeeRepository.save(transientEmployee);
-                employees.put(persistentEmployee.getId(),persistentEmployee);
-            } else {
-                Employee persistentEmployee = employeeRepository.getById(reportSerializer.getId());
-                employees.put(persistentEmployee.getId(),persistentEmployee);
-            }
-        }
-
-        /*
-        for (EmployeeReportSerializer reportSerializer : reportSerializers) {
-
-            Employee persitentEmployee = employeeRepository.getById(reportSerializer.getId());
-            if (persitentEmployee == null) {
-                Employee transientEmployee = Employee.builder()
-                        .id(reportSerializer.getId())
-                        .department(reportSerializer.getDepartment())
-                        .build();
-                employeeRepository.save(transientEmployee);
-                employees.add(transientEmployee);
-
-            } else {
-                employees.add(persitentEmployee);
-            }
-
-        }
-        */
-
-        return employees;
     }
 }
