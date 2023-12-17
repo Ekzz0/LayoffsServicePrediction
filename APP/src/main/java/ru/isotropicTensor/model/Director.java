dda1214303b9a8cd6116d3b779057957f0709ed7
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2023-12-17 17:32:26 +0300

    Added Director entity class and repository for it.

---
+++ APP/src/main/java/ru/isotropicTensor/model/Director.java
@@ -0,0 +1,29 @@
+package ru.isotropicTensor.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "director")
+public class Director {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String department;
+    private String email;
+}
---
+++ APP/src/main/java/ru/isotropicTensor/repository/DirectorRepository.java
@@ -0,0 +1,7 @@
+package ru.isotropicTensor.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.isotropicTensor.model.Director;
+
+public interface DirectorRepository extends JpaRepository<Integer, Director> {
+}
