ec2b7af68f7bd728a076b50b9b73584757320ffb
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2023-12-17 21:17:08 +0300

    Added method into DirectorRepository

--- APP/src/main/java/ru/isotropicTensor/repository/DirectorRepository.java
+++ APP/src/main/java/ru/isotropicTensor/repository/DirectorRepository.java
@@ -3,5 +3,9 @@ package ru.isotropicTensor.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.isotropicTensor.model.Director;
 
+import java.util.List;
+import java.util.Set;
+
 public interface DirectorRepository extends JpaRepository<Director, Integer> {
+    List<Director> findByDepartmentIn(Set<String> departments);
 }
