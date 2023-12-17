039616b1061e161ed55d24c31c915a5de7ddb719
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2023-12-17 14:47:10 +0300

    Added query method for EmployeePredictionRepository

--- APP/src/main/java/ru/isotropicTensor/repository/EmployeePredictionRepository.java
+++ APP/src/main/java/ru/isotropicTensor/repository/EmployeePredictionRepository.java
@@ -2,6 +2,7 @@ package ru.isotropicTensor.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.isotropicTensor.model.EmployeePrediction;
 
 import java.time.LocalDateTime;
@@ -10,4 +11,8 @@ import java.util.List;
 public interface EmployeePredictionRepository extends JpaRepository<EmployeePrediction, Integer> {
     @Query("SELECT ep.date FROM EmployeePrediction ep GROUP BY ep.date")
     List<LocalDateTime> findAllByDate();
+
+    @Query("SELECT ep FROM EmployeePrediction ep WHERE ep.date = :date")
+    List<EmployeePrediction> findByDate(@Param("date") LocalDateTime date);
+
 }
