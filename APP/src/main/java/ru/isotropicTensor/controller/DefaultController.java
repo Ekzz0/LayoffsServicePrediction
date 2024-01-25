91234de849627eba9873ddb63a83924e873e2dde
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2024-01-25 18:13:54 +0300

    Added origin into controllers

--- APP/src/main/java/ru/isotropicTensor/controller/DefaultController.java
+++ APP/src/main/java/ru/isotropicTensor/controller/DefaultController.java
@@ -1,9 +1,11 @@
 package ru.isotropicTensor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 public class DefaultController {
     @GetMapping("/")
     public String index() {
