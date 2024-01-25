7968790129a295b6856268dec0b1d7c877e32379
Алексей <zaykuh@gmail.com>
2024-01-25 18:29:21 +0500

    init: react initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+
+
+
+function App() {
+  async function handleRequest(link, request, json=undefined){
+
+    return fetch(link, {
+        method: request,
+        body: JSON.stringify(json),
+        headers: {"Content-Type": 'application/json; charset=UTF-8'},
+    })
+    .then(response =>{
+        if (response.ok){
+           return response.text()
+        }
+        else{
+            alert(`Не выполнен ${request}`)
+            throw(`Failed in ${request} request ${link}`)
+        }
+    })
+    .catch(error => {
+        throw(`Ошибка: ${error}`)
+    })
+  }
+  
+  return (
+    <div>
+     <button onClick={test}>тест</button>
+    </div>
+  );
+}
+
+export default App;
