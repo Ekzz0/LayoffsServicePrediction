568d4f2a7da616111a399ee398717636b9833e13
Алексей <zaykuh@gmail.com>
2024-02-06 15:48:39 +0500

    init: initial react project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,13 @@
-
+import { Main } from "./layout/Main";
+import { Sidebar } from "./layout/Sidebar";
+import "./scss/index.scss"
 
 function App() {
-  async function handleRequest(link, request, json=undefined){
-
-    return fetch(link, {
-        method: request,
-        body: JSON.stringify(json),
-        headers: {"Content-Type": 'application/json; charset=UTF-8'},
-    })
-    .then(response =>{
-        if (response.ok){
-           return response.text()
-        }
-        else{
-            alert(`Не выполнен ${request}`)
-            throw(`Failed in ${request} request ${link}`)
-        }
-    })
-    .catch(error => {
-        throw(`Ошибка: ${error}`)
-    })
-  }
   
-  function test() {
-    handleRequest('http://localhost:8081/api/get-history', 'GET')
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-  
-
   return (
-    <div>
-     <button onClick={test}>тест</button>
+    <div className="container">
+      <Sidebar/>
+      <Main/>
     </div>
   );
 }
