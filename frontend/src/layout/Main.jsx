fd827a05650f5c947713882446c48dbb7242f133
Алексей <zaykuh@gmail.com>
2024-02-06 16:19:18 +0500

    feat: add Main.jsx

---
+++ frontend/src/layout/Main.jsx
@@ -0,0 +1,11 @@
+import { Content } from "./Content"
+import { Header } from "./Header"
+
+function Main(){
+    return <div className="main">
+        <Header/>
+        <Content/>
+    </div>
+}
+
+export { Main }
