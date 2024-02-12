f8de330f35144d299cce1a7e8950fc0d4ce05c98
Алексей <zaykuh@gmail.com>
2024-02-12 16:37:02 +0500

    refactor: ref Card.jsx

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,12 +2,12 @@ import frame from '../images/frame.svg'
 
 
 function Card({id, probability, departament}){
-
+    
     return <div className="card">
-        <h2>{id}</h2>
+        <h2 className='card-id'>{id}</h2>
         <img src={frame} alt="frame" />
-        <p>{probability}%</p>
-        <p>{departament}</p>
+        <p className='card-prob'>{probability}%</p>
+        <p className='card-dep'>{departament}</p>
     </div>
 }
 
