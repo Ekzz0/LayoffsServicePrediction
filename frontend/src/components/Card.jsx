84f709f15a24ac36b01a37db78f188cb7dd3366e
Алексей <zaykuh@gmail.com>
2024-02-26 02:23:38 +0500

    feat: add handle click

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,14 +1,19 @@
 import frame from '../images/frame.svg'
 
 
-function Card({id, probability, departament}){
-    
-    return <div className="card">
+function Card({id, probability, departament, openModal}){
+
+    function handleClick(){
+        openModal(id)
+    }
+
+    return <div onClick={handleClick} className="card">
         <h2 className='card-id'>{id}</h2>
-        <img src={frame} alt="frame" />
+        <img className='card-img' src={frame} alt="frame" />
         <p className='card-prob'>{probability}%</p>
         <p className='card-dep'>{departament}</p>
     </div>
+    
 }
 
 export { Card }
